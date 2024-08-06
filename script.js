0d88a39b824fbc459665116a88e485fef77c3e3d
Madhu Pawar <148059576+madhuap555@users.noreply.github.com>
2024-08-06 23:01:29 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript for interactive elements if needed
+document.getElementById('menu-icon').onclick = function() {
+    var navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+  }
+  
